ac35b55aff79111331edb1bd3f942220954d30b6
IvanHuayta <107371442+IvanHuayta@users.noreply.github.com>
2025-10-14 00:24:22 -0400

    feat(nav+footer): agregar navegacion y pie de pagina

--- manaco/src/app/layout/layout.ts
+++ manaco/src/app/layout/layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
-  templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  imports: [RouterOutlet],
+  templateUrl: './layout.html'
 })
 export class Layout {
-
+  
 }
